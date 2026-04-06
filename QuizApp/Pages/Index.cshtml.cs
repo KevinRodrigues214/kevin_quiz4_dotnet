02fdc9162cdc93372169eb2c58a995a7419372ea
Kevin Rodrigues <a.kevinrodrigues@gmail.com>
2026-04-06 09:30:18 -0400

    Added database access functionality - displays message on web page

--- QuizApp/Pages/Index.cshtml.cs
+++ QuizApp/Pages/Index.cshtml.cs
@@ -1,12 +1,21 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace QuizApp.Pages;
-
-public class IndexModel : PageModel
+namespace QuizApp.Pages
 {
-    public void OnGet()
+    public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        public string DatabaseMessage { get; set; }
 
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DatabaseMessage = "Database access has already been done on April 6, 2026 by [SEU NOME COMPLETO AQUI]";
+        }
     }
 }
